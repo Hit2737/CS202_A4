8f8e1af6e30cbdf2346ddc96abc82ec720737f61
Hit2737 <hitesh.kumar@iitgn.ac.in>
2025-04-22 17:43:35 +0530

    added code for assignment 4

---
+++ ConsoleAlarmTimer/ConsoleAlarmTimer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace AlarmClockConsoleApp
+{
+    // Subscriber class containing the event handler
+    class Subscriber_class
+    {
+        public static void Ring()
+        {
+            Console.WriteLine("ALARM!!!!!!!!!!!!!");
+            Thread.Sleep(1000);
+        }
+    }
+
+    // Publisher class raising the event
+    class Publisher_class
+    {
+        public event Alarm RaiseEvent;        // Event declaration
+        public delegate void Alarm();         // Delegate definition
+
+        public void SetTime(string sTime)
+        {
+            string currTime = DateTime.Now.ToString("HH:mm:ss"); // match format exactly
+            while (currTime != sTime)
+            {
+                Console.WriteLine("Tick-Tock...");
+                Thread.Sleep(1000);
+                currTime = DateTime.Now.ToString("HH:mm:ss");
+            }
+
+            RaiseEvent(); // raise the event
+        }
+
+        static void Main(string[] args)
+        {
+            Publisher_class p = new Publisher_class();
+            p.RaiseEvent += new Alarm(Subscriber_class.Ring);
+
+            Console.WriteLine("Enter Time for Alarm (HH:mm:ss) =>");
+            string t = Console.ReadLine();
+
+            // ✅ Input format check
+            if (TimeSpan.TryParseExact(t, "hh\\:mm\\:ss", null, out _))
+            {
+                p.SetTime(t);
+            }
+            else
+            {
+                Console.WriteLine("❌ Invalid format! Please enter time in HH:mm:ss format.");
+            }
+        }
+    }
+}
---
+++ WindowsFormsAlarmTimer/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace STT_Lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(206, 45);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Enter";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(31, 45);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(143, 21);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "HH:MM:SS";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 9F, FontStyle.Bold);
+            label1.Location = new Point(31, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Enter Time ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(315, 109);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Font = new Font("Times New Roman", 9F);
+            Name = "Form1";
+            Text = "Alarm Form";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+    }
+}
